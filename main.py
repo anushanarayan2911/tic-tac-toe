595f93437acddbbd7802baa163c0f3da6e2f8894
anushanarayan2911 <anusha.narayan2002@outlook.com>
2020-08-10 11:29:04 +0100

    Initialise blank screen

---
+++ main.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+def main():
+    screen = Tk()
+    screen.title("Tic Tac Toe")
+    screen.geometry("500x500")
+    
+    screen.mainloop()
+
+if __name__ == "__main__":
+    main()
